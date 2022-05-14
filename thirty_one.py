82564a5deedad5a3a833aff63f6d729bcca76903
Langelozzi <lucasangelozzi32@gmail.com>
2022-05-13 22:05:36 -0700

    added knock feature for computer and made function able to change computer number for print outs

--- thirty_one.py
+++ thirty_one.py
@@ -7,6 +7,8 @@ from cards_module.standard_deck import Deck
 from thirty_one_hand import TO_Hand
 from cards_module.card import Card
 from collections import Counter
+import os
+import time
 
 def create_deck() -> list:
     deck = Deck()
@@ -30,6 +32,7 @@ def calculate_hand_total(hand: list) -> int:
     else:
         return max([card.value for card in hand])
 
+
 def determine_worst_card(hand: list) -> Card:
     suits_in_hand = [card.suit for card in hand]
     
@@ -44,21 +47,31 @@ def determine_worst_card(hand: list) -> Card:
         # returning the card object that has the lowest value of all the cards
         return [card for card in hand if card.value == min([card.value for card in hand])][0]
 
-# work in progress
-def comp_turn(hand: TO_Hand, disc_card: Card, disc_pile: list, deck: list, deck_card: Card, k: bool) -> bool:
-    print("\nComputer 1 is taking their turn...\n")
+
+def comp_turn(numb: int, hand: TO_Hand, disc_card: Card, disc_pile: list, deck: list, deck_card: Card, k: int) -> bool:
+    print(f"\nComputer {numb} is taking their turn...")
+    time.sleep(1)
 
     hand_w_disc = hand.hand.copy()
     hand_w_disc.append(disc_card)
     disc_pile.remove(disc_card)
 
     if calculate_hand_total(hand_w_disc) > hand.calculate_hand_total():
+        print(f"Computer {numb} drew from the discard pile")
+        time.sleep(1)
+        print(f"Computer {numb} has discarded a card")
+        time.sleep(1)
         hand = hand_w_disc
 
         worst_card = determine_worst_card(hand)
         hand.remove(worst_card)
         disc_pile.append(worst_card)
+    
     else:
+        print(f"Computer {numb} drew from the deck")
+        time.sleep(1)
+        print(f"Computer {numb} has discarded a card\n")
+        time.sleep(1)
         hand.add_card(deck_card)
         
         if deck_card in deck:
@@ -70,15 +83,16 @@ def comp_turn(hand: TO_Hand, disc_card: Card, disc_pile: list, deck: list, deck_
 
         hand = hand.hand
 
-    if not k or k == False:
+    if k == 1:
         hand_total = calculate_hand_total(hand)
         if hand_total > 10:
-            print("Computer 1 has knocked, you have one turn left\n")
+            print(f"Computer {numb} has knocked, you have one turn left\n")
+            time.sleep(3)
             return True
         else:
             return False
 
-def player_turn(player_hand: TO_Hand, discard_card: Card, discard_pile: list, deck: list, top_deck_card: Card, k: bool):
+def player_turn(player_hand: TO_Hand, discard_card: Card, discard_pile: list, deck: list, top_deck_card: Card, k: int):
     print(f"<---------- Your Cards ---------->")
     player_hand.print_hand()
     print(f"Current Hand Value: {player_hand.calculate_hand_total()}\n")
@@ -107,7 +121,7 @@ def player_turn(player_hand: TO_Hand, discard_card: Card, discard_pile: list, de
     discard_card = player_hand.remove_card(player_hand.hand[discard_option-1])
     discard_pile.append(discard_card)
     
-    if not k or k == False:
+    if k == 1:
         knocked = input("\nWould you like to knock?: ")
         if knocked.lower() in ("yes", "y", "ye", "yea", "yeah"):
             return True, discard_card, discard_pile, deck, top_deck_card
@@ -116,8 +130,16 @@ def player_turn(player_hand: TO_Hand, discard_card: Card, discard_pile: list, de
 
     return False, discard_card, discard_pile, deck, top_deck_card
 
+def clear_console():
+    # if on windows
+    if os.name in ('nt', 'dos'):
+        os.system('cls')
+    # if on mac or linux
+    else:
+        os.system('clear')
 
 def main() -> None:
+    clear_console()
     deck = create_deck()
     player_hand = TO_Hand(deck)
     comp1_hand = TO_Hand(deck)
@@ -135,8 +157,13 @@ def main() -> None:
         top_deck_card = deck[::-1][0]
         discard_card = discard_pile[::-1][0]
         
-        knocked, discard_card, discard_pile, deck, top_deck_card = player_turn(player_hand, discard_card, discard_pile, deck, top_deck_card, knocked)
-        knocked = comp_turn(comp1_hand, discard_card, discard_pile, deck, top_deck_card, knocked)
+        knocked, discard_card, discard_pile, deck, top_deck_card = player_turn(player_hand, discard_card, discard_pile, deck, top_deck_card, knock)
+        clear_console()
+        knocked = comp_turn(1, comp1_hand, discard_card, discard_pile, deck, top_deck_card, knock)
+        # time.sleep(2)
+        clear_console()
+
+    
         
 
 if __name__ == "__main__":
